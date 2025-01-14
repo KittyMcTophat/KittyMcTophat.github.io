9fc081f56bc6a1e2e31885cbd549c3bec3e4b463
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2025-01-13 20:34:56 -0700

    Move Smol Yuro onclick functionality to `squeak()` function

--- scripts/easterEggs.js
+++ scripts/easterEggs.js
@@ -1,12 +1,4 @@
-$(() => {
-    initializeSmolYuri();
-});
-
-function initializeSmolYuri() {
-    let $SmolYuri = $("#SmolYuriImg");
-
-    $SmolYuri.on("click", () => {
-        let clickSound = new Audio("./sounds/squeak.mp3");
-        clickSound.play();
-    });
+function squeak() {
+    let squeakSound = new Audio("./sounds/squeak.mp3");
+    squeakSound.play();
 }
